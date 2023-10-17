c4f87f0e84f1f174f9c268b25f8c47cc86054571
svtter <svtter@qq.com>
2023-10-18 00:03:58 +0800

    feat: 增加时间戳和注释

--- src/base/models.py
+++ src/base/models.py
@@ -14,8 +14,8 @@ class IDHandler:
 
 class TimeModel(models.Model, IDHandler):
     id: int
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_time = models.DateTimeField(auto_now=True, verbose_name="更新时间")
 
     class Meta:
         abstract = True
--- src/project/models.py
+++ src/project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from base.models import TimeModel
+from repos.models import TemplateRepo
 
 # Create your models here.
+
+
+class Project(TimeModel):
+    name = models.CharField(max_length=100, verbose_name="项目名称")
+    template = models.ForeignKey(TemplateRepo, on_delete=models.DO_NOTHING, verbose_name="模板")

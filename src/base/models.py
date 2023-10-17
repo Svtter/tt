f97451c8edd3fba528e19375c1c3e71e0341433d
svtter <svtter@qq.com>
2023-10-17 23:58:45 +0800

    feat: add project

--- src/base/models.py
+++ src/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from typing import Type
+from abc import abstractmethod
 
-# Create your models here.
+
+class IDHandler:
+    @abstractmethod
+    def is_valid(self, update_params, model_instance, class_name, defined_exception: Type[Exception]):
+        if update_params.get("id") and update_params.get("id") != model_instance.id:
+            raise defined_exception(
+                f'The {class_name}.id {model_instance.id} is not equal to model instance {update_params.get("id")}.'
+            )
+
+
+class TimeModel(models.Model, IDHandler):
+    id: int
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def get_id(self) -> int:
+        return self.id
--- src/repos/models.py
+++ src/repos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from base.models import TimeModel
 
 # Create your models here.
+
+
+class TemplateRepos(TimeModel):
+    name = models.CharField(max_length=100, verbose_name="名称")
+    repos = models.CharField(max_length=100, verbose_name="仓库地址")
+    content = models.TextField("内容", blank=True, null=True)

ac4d5efbd61330c72ea824b9f34b95f3b6a0d26a
svtter <svtter@qq.com>
2023-10-18 09:29:51 +0800

    feat: redirect home page to repos

--- src/conf/urls.py
+++ src/conf/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("base.urls"), name="base"),
     path("admin/", admin.site.urls),
     path("repos/", include("repos.urls"), name="repos"),
 ]
